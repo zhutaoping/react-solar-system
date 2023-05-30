8a33b524aac635cdf3fc337415d62f755629db6d
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-05-31 04:29:08 +0800

    Change png to webp

--- src/pages/jupiter/JupiterVideo.tsx
+++ src/pages/jupiter/JupiterVideo.tsx
@@ -1,6 +1,9 @@
-import jupiterImage from '../../assets/images/jupiter-720.png'
+import jupiterMedia from '/assets/images/jupiter-720-tiny.webp'
 import Video from '../../components/Video'
 
 export default function JupiterVideo() {
-  return <Video src={jupiterImage} isImage />
+  const media = new Image()
+  media.src = jupiterMedia
+  media.alt = 'Jupiter'
+  return <Video media={media} isImage />
 }
