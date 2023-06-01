cae258582ec2872165d6e6362b71d6fa54d25d02
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-06-02 02:38:46 +0800

    Change mercury video to webp

--- src/pages/mercury/MercuryVideo.tsx
+++ src/pages/mercury/MercuryVideo.tsx
@@ -1,5 +1,5 @@
 import Video from '../../components/Video'
-import mercuryVideo from '/assets/videos/mercury-720-low.mp4'
+import mercuryVideo from '/assets/videos/mercury-720.webm'
 import mercuryPoster from '/assets/images/posters/mercury-poster-720-tiny.webp'
 
 export default function MercuryVideo() {
