910d1c17de7fa8ee28a9e102bf72d1ca7d40703b
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-06-02 23:27:40 +0800

    Replace sun poster to 1080

--- src/pages/home/SunVideo.tsx
+++ src/pages/home/SunVideo.tsx
@@ -1,6 +1,6 @@
 import Video from '../../components/Video'
 import video from '/assets/videos/sun-clip.webm'
-import poster from '/assets/images/posters/sun-poster-720-tiny.webp'
+import poster from '/assets/images/posters/sun-poster-1080-tiny.webp'
 
 export default function SunVideo() {
   return <Video videoSrc={video} videoPoster={poster} isSun />
