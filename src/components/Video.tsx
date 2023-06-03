5a655efefcccb0499cdc70b3b114733d53ae1a9a
zhutaoping <97973338+zhutaoping@users.noreply.github.com>
2023-06-03 22:48:21 +0800

    try something

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -66,7 +66,7 @@ export default function Video({
             }}
             className={`${
               isSun ? 'isSun h-screen object-cover portrait:h-full ' : ''
-            } ${canPlay ? 'hidden' : ''}`}
+            } ${canPlay && posterCompleted ? 'hidden' : ''}`}
             src={videoPoster}
             alt={videoPoster}
           />
